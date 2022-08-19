1e88cbe7bd9904d085d93f70b5da5520e926f7a6
Douglas Obara <douglasobara97@gmail.com>
2022-08-19 23:59:48 +0300

    commit tree c45207cd6c2c33348d5355443bf27dacca209007

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
+# returns a string without any additional formatting when given a 1-element array 
+# adds 'and' between elements when given a 2-element array 
+# adds commas plus a final 'and' when given a 3-element array 
+# correctly formats arrays of lengths greater than three 
 def oxford_comma(array)
+  return array.join(" and ") if array.size < 3
 
+  # insert 'and ' in front of the last string in the array
+  array[-1] = "and #{array[-1]}"
+
+  array.join(", ") 
 end
